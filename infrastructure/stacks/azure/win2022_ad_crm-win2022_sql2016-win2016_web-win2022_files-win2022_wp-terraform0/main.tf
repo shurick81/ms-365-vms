278e04baaaa6ff2e6c6913e7081f57d90eaa20d3
Aleks <aleksandr.sapozhkov@soprasteria.com>
2023-02-15 10:15:36 +0100

    adding local admin users to DB

--- infrastructure/stacks/azure/win2022_ad_crm-win2022_sql2016-win2016_web-win2022_files-win2022_wp-terraform0/main.tf
+++ infrastructure/stacks/azure/win2022_ad_crm-win2022_sql2016-win2016_web-win2022_files-win2022_wp-terraform0/main.tf
@@ -110,7 +110,7 @@ module "DB00" {
   vm_name                             = "${format(var.MS_365_VMS_VM_NAME_SPEC, "db00")}"
   vm_size                             = "${var.MS_365_VMS_WIN2022_SQL2016_VM_SIZE}"
   ms_365_vms_domain_name              = "${var.MS_365_VMS_DOMAIN_NAME}"
-  local_admins                        = ""
+  local_admins                        = "${var.MS_365_VMS_DOMAIN_NAME}\\CRM Administrators 00,${var.MS_365_VMS_DOMAIN_NAME}\\_crmdplsrv"
   dependencies = [
     "${module.AD00.depended_on}"
   ]
