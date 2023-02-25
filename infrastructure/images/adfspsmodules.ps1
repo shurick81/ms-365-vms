c1eaf3bc85f0c35941757c6491478821e4dc682a
Aleks <aleksandr.sapozhkov@soprasteria.com>
2023-02-25 11:12:58 +0100

    win2016-adfs image

---
+++ infrastructure/images/adfspsmodules.ps1
@@ -0,0 +1,91 @@
+$configName = "ADFSPSModules";
+Write-Host "$(Get-Date) Defining DSC";
+try
+{
+    Configuration $configName
+    {
+        param(
+        )
+
+        Import-DscResource -ModuleName PSDesiredStateConfiguration
+        Import-DscResource -ModuleName PackageManagementProviderResource -ModuleVersion 1.0.3
+
+        Node $AllNodes.NodeName
+        {
+            
+            PSModule "PSModule_AdfsDsc"
+            {
+                Ensure              = "Present"
+                Name                = "AdfsDsc"
+                Repository          = "PSGallery"
+                InstallationPolicy  = "Trusted"
+                RequiredVersion     = "1.0.0"
+            }
+
+            PSModule "PSModule_ADFSToolbox"
+            {
+                Ensure              = "Present"
+                Name                = "ADFSToolbox"
+                Repository          = "PSGallery"
+                InstallationPolicy  = "Trusted"
+                RequiredVersion     = "2.0.17.0"
+            }
+
+        }
+    }
+}
+catch
+{
+    Write-Host "$(Get-Date) Exception in defining DCS:";
+    $_.Exception.Message;
+    Exit 1;
+}
+$configurationData = @{ AllNodes = @(
+    @{ NodeName = $env:COMPUTERNAME; PSDscAllowPlainTextPassword = $true; PsDscAllowDomainUser = $true }
+) }
+Write-Host "$(Get-Date) Compiling DSC";
+try
+{
+    &$configName `
+        -ConfigurationData $configurationData;
+}
+catch
+{
+    Write-Host "$(Get-Date) Exception in compiling DCS:";
+    $_.Exception.Message;
+    Exit 1;
+}
+Write-Host "$(Get-Date) Starting DSC";
+try
+{
+    Start-DscConfiguration $configName -Verbose -Wait -Force;
+}
+catch
+{
+    Write-Host "$(Get-Date) Exception in starting DCS:";
+    $_.Exception.Message;
+    Exit 1;
+}
+if ( $env:VMDEVOPSSTARTER_NODSCTEST -ne "TRUE" )
+{
+    Write-Host "$(Get-Date) Testing DSC";
+    try {
+        $result = Test-DscConfiguration $configName -Verbose;
+        $inDesiredState = $result.InDesiredState;
+        $failed = $false;
+        $inDesiredState | % {
+            if ( !$_ ) {
+                Write-Host "$(Get-Date) Test failed";
+                Exit 1;
+            }
+        }
+    }
+    catch {
+        Write-Host "$(Get-Date) Exception in testing DCS:";
+        $_.Exception.Message;
+        Exit 1;
+    }
+} else {
+    Write-Host "$(Get-Date) Skipping tests";
+}
+Exit 0;
