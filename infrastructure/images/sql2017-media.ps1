4e8c7521b95134bb2ff6d13d72cd4356de352595
Aleks <aleksandr.sapozhkov@soprasteria.com>
2023-03-22 23:04:50 +0100

    win2016-sql2017-rs image

---
+++ infrastructure/images/sql2017-media.ps1
@@ -0,0 +1,133 @@
+$configName = "SQLMedia";
+Write-Host "$(Get-Date) Defining DSC";
+try
+{
+    Configuration $configName
+    {
+        param(
+        )
+    
+        Import-DscResource -ModuleName PSDesiredStateConfiguration
+        Import-DscResource -ModuleName xPSDesiredStateConfiguration -Name xRemoteFile -ModuleVersion 8.4.0.0
+        Import-DscResource -ModuleName StorageDsc -ModuleVersion 4.9.0.0
+    
+        Node $AllNodes.NodeName
+        {
+    
+            #$SQLImageUrl = "https://download.microsoft.com/download/E/F/2/EF23C21D-7860-4F05-88CE-39AA114B014B/SQLServer2017-x64-ENU-Dev.iso";
+            $SQLImageUrl = "https://download.microsoft.com/download/E/F/2/EF23C21D-7860-4F05-88CE-39AA114B014B/SQLServer2017-x64-ENU-Dev.iso";
+            $SQLImageUrl -match '[^/\\&\?]+\.\w{3,4}(?=([\?&].*$|$))' | Out-Null
+            $SQLImageFileName = $matches[0]
+            $SQLImageDestinationPath = "C:\Install\SQLRTMImage\$SQLImageFileName"
+    
+            xRemoteFile SQLServerImageFilePresent
+            {
+                Uri             = $SQLImageUrl
+                DestinationPath = $SQLImageDestinationPath
+                MatchSource     = $false
+            }
+    
+            MountImage SQLServerImageMounted
+            {
+                ImagePath   = $SQLImageDestinationPath
+                DriveLetter = 'F'
+                DependsOn   = "[xRemoteFile]SQLServerImageFilePresent"
+            }
+    
+            WaitForVolume SQLServerImageMounted
+            {
+                DriveLetter         = 'F'
+                RetryIntervalSec    = 5
+                RetryCount          = 10
+                DependsOn           = "[MountImage]SQLServerImageMounted"
+            }
+
+            xRemoteFile SQLServerCUFilePresent
+            {
+                Uri             = "https://download.microsoft.com/download/C/4/F/C4F908C9-98ED-4E5F-88D5-7D6A5004AEBD/SQLServer2017-KB5016884-x64.exe"
+                DestinationPath = "C:\Install\SQLUpdates\SQLServer2017-KB5016884-x64.exe"
+                MatchSource     = $false
+            }
+    
+            xRemoteFile SQLServerRSFilePresent
+            {
+                Uri             = "https://download.microsoft.com/download/E/6/4/E6477A2A-9B58-40F7-8AD6-62BB8491EA78/SQLServerReportingServices.exe"
+                DestinationPath = "C:\Install\SQLRTMImage\SQLServerReportingServices.exe"
+                MatchSource     = $false
+            }
+
+        }
+    }
+}
+catch
+{
+    Write-Host "$(Get-Date) Exception in defining DCS:";
+    $_.Exception.Message;
+    Exit 1;
+}
+$configurationData = @{ AllNodes = @(
+    @{ NodeName = $env:COMPUTERNAME; PSDscAllowPlainTextPassword = $true; PsDscAllowDomainUser = $true }
+) }
+Write-Host "$(Get-Date) Compiling DSC";
+try
+{
+    &$configName `
+        -ConfigurationData $configurationData;
+}
+catch
+{
+    Write-Host "$(Get-Date) Exception in compiling DCS:";
+    $_.Exception.Message;
+    Exit 1;
+}
+Write-Host "$(Get-Date) Starting DSC";
+try
+{
+    if ( $env:VMDEVOPSSTARTER_NODSCWAIT )
+    {
+        Start-DscConfiguration $configName -Verbose -Force;
+        Sleep 20;
+        0..720 | % {
+            $res = Get-DscLocalConfigurationManager;
+            Write-Host $res.LCMState;
+            if ( ( $res.LCMState -ne "Idle" ) -and ( $res.LCMState -ne "PendingConfiguration" ) ) {
+                Sleep 10;
+            }
+        }
+        if ( ( $res.LCMState -ne "Idle" ) -and ( $res.LCMState -ne "PendingConfiguration" ) ) {
+            Write-Host "Timouted waiting for LCMState"
+            Exit 1;
+        }
+    } else {
+        Start-DscConfiguration $configName -Verbose -Wait -Force;
+    }
+}
+catch
+{
+    Write-Host "$(Get-Date) Exception in starting DCS:";
+    $_.Exception.Message;
+    Exit 1;
+}
+if ( $env:VMDEVOPSSTARTER_NODSCTEST -ne "TRUE" )
+{
+    Write-Host "$(Get-Date) Testing DSC";
+    try {
+        $result = Test-DscConfiguration $configName -Verbose;
+        $inDesiredState = $result.InDesiredState;
+        $failed = $false;
+        $inDesiredState | % {
+            if ( !$_ ) {
+                Write-Host "$(Get-Date) Test failed";
+                Exit 1;
+            }
+        }
+    }
+    catch {
+        Write-Host "$(Get-Date) Exception in testing DCS:";
+        $_.Exception.Message;
+        Exit 1;
+    }
+} else {
+    Write-Host "$(Get-Date) Skipping tests";
+}
+Exit 0;
